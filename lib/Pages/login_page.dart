b937a738c3a9b578948a4b50cd80806e59d6ca07
VishalCodiste <vishal.p@codiste.com>
2023-10-13 17:33:09 +0530

    Learn AnimatedContainer, InkWell, StatefulWidget and StatelessWidget diffrence, use icon, await, navigate other page, ElevatedButton, etc....

--- lib/Pages/login_page.dart
+++ lib/Pages/login_page.dart
@@ -3,7 +3,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutterlearning/utils/routs.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  String name = "";
+  bool changeLoginBtn = false;
+
   @override
   Widget build(BuildContext context) {
     return Material(
@@ -17,8 +25,8 @@ class LoginPage extends StatelessWidget {
               fit: BoxFit.cover,
             ),
             const SizedBox(height: 10),
-            const Text(
-              "Welcome",
+            Text(
+              "Welcome $name",
               style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
             ),
             const SizedBox(height: 10),
@@ -29,6 +37,10 @@ class LoginPage extends StatelessWidget {
                   TextFormField(
                     decoration: const InputDecoration(
                         hintText: "Enter username", labelText: "Username"),
+                    onChanged: (value) {
+                      name = value;
+                      setState(() {});
+                    },
                   ),
                   TextFormField(
                     obscureText: true,
@@ -36,12 +48,45 @@ class LoginPage extends StatelessWidget {
                         hintText: "Enter Password", labelText: "Password"),
                   ),
                   const SizedBox(height: 30),
-                  ElevatedButton(
-                      style: TextButton.styleFrom(minimumSize: Size(150, 40)),
-                      onPressed: () {
-                        Navigator.pushNamed(context, MyRouts.homePage);
-                      },
-                      child: const Text("Login"))
+
+                  InkWell(
+                    hoverColor: Colors.white,
+                    onTap: () async {
+                      setState(() {
+                        changeLoginBtn = true;
+                      });
+                      
+                      await Future.delayed(const Duration(seconds: 1));
+                      Navigator.pushNamed(context, MyRouts.homePage);
+                    },
+                    child: AnimatedContainer(
+                      duration: Duration(seconds: 1),
+                      width: changeLoginBtn ? 50 : 150,
+                      height: 50,
+                      alignment: Alignment.center,
+                      decoration: BoxDecoration(
+                        color: Colors.deepPurple,
+                        borderRadius:
+                            BorderRadius.circular(changeLoginBtn ? 25 : 8),
+                      ),
+                      child: changeLoginBtn
+                          ? const Icon(Icons.done, color: Colors.white)
+                          : const Text(
+                              "Login",
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 18),
+                            ),
+                    ),
+                  )
+                  // ElevatedButton(
+                  //   style: TextButton.styleFrom(minimumSize: Size(150, 40)),
+                  //   onPressed: () {
+                  //     Navigator.pushNamed(context, MyRouts.homePage);
+                  //   },
+                  //   child: const Text("Login")
+                  // )
                 ],
               ),
             ),
